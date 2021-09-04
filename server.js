cd6b5ff076eab4988809e79e5c22120164f11c12
cb-co <cmcorporations.adm@gmail.com>
2021-09-04 17:25:54 -0400

    package update and post endpoint

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
 
-var app = express();
+require('dotenv').config();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Endpoints
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { originalname, mimetype, size } = req.file;
 
-
+  res.json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
